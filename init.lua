a19fa481c65fbf25b971f189326db11431af4581
ccolorado <carlos.colorado@gmail.com>
2024-08-16 23:02:59 -0600

    Cleanup

--- init.lua
+++ init.lua
@@ -1005,12 +1005,12 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-  require 'kickstart.plugins.noice', -- adds gitsigns recommend keymaps
 
   -- [[ CUSTOM Settings ]]
   require 'custom.mappings',
+  require 'custom.autocorrect',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
